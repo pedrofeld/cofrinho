be9703f0d0d592a1121cbcb6ed847295b50b155e
Pedro Feld <pedro.gael.feld@gmail.com>
2025-11-28 21:06:07 -0300

    refactor: resetando a classe principal

--- Cofrinho/src/uninter/Principal.java
+++ Cofrinho/src/uninter/Principal.java
@@ -1,19 +1,9 @@
 package uninter;
 
-import java.util.ArrayList;
-
 public class Principal {
 
 	public static void main(String[] args) {
-		ArrayList<Moeda> lista = new ArrayList<Moeda>();
-		
-		lista.add(new Real(1000));
-		lista.add(new Dolar(100));
-		lista.add(new Euro(10));
-		
-		for( Moeda o : lista ) {
-			o.info();
-		}
+		// TODO
 	}
 
 }
